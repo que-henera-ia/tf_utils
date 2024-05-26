550d659206677b1e646854bd5b073c1bc60a1651
victorfdezc <victorfdezc1996@gmail.com>
2024-05-26 22:24:02 +0200

    cvae working

--- manage_data.py
+++ manage_data.py
@@ -4,22 +4,74 @@ import matplotlib.pyplot as plt
 import PIL.Image
 import imageio
 import glob
+import os
 
-def load_img(path_to_img, img_shape=512):
-  max_dim = img_shape
+def load_img(path_to_img, img_shape=512, img_channels=3, preserve_aspect_ratio=False):
   img = tf.io.read_file(path_to_img)
-  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.decode_image(img, channels=img_channels)
   img = tf.image.convert_image_dtype(img, tf.float32)
 
-  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
-  long_dim = max(shape)
-  scale = max_dim / long_dim
-
-  new_shape = tf.cast(shape * scale, tf.int32)
+  if preserve_aspect_ratio:
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = img_shape / long_dim
+    new_shape = tf.cast(shape * scale, tf.int32)
+  else:
+    new_shape = tf.cast([img_shape,img_shape], tf.int32)
+  
 
   img = tf.image.resize(img, new_shape)
   img = img[tf.newaxis, :]
   return img
+
+def load_img_dataset(path_to_dataset, img_shape=512, img_channels=3, preserve_aspect_ratio=False):
+  # Check if the path exists
+  if not os.path.exists(path_to_dataset):
+    print("The specified path does not exist.")
+    return
+  # Check if the path is a directory
+  if not os.path.isdir(path_to_dataset):
+    print("The specified path is not a directory.")
+    return
+  # List all files in the directory
+  files = os.listdir(path_to_dataset)
+  
+  dataset = []
+  for file in files:
+    img = load_img(os.path.join(path_to_dataset, file), img_shape=img_shape, img_channels=img_channels,preserve_aspect_ratio=preserve_aspect_ratio)
+    dataset.append(img[0])
+  return tf.convert_to_tensor(dataset)
+
+
+def extract_video_frames(path_to_video, img_shape=512 ,img_channels=3, preserve_aspect_ratio=False):
+  video_reader = imageio.get_reader(path_to_video)
+  frames = []
+  for frame in video_reader:
+    # Convert frame to PIL Image
+    pil_frame = PIL.Image.fromarray(frame)
+    # Convert to grayscale
+    if img_channels==1: 
+      pil_frame = pil_frame.convert('L')
+    
+    pil_frame = tf.convert_to_tensor(np.array(pil_frame))
+
+    if img_channels==1:
+      # Extend dimension if grayscale
+      pil_frame = pil_frame[..., tf.newaxis]
+
+    if preserve_aspect_ratio:
+      shape = tf.cast(tf.shape(pil_frame)[:-1], tf.float32)
+      long_dim = max(shape)
+      scale = img_shape / long_dim
+      new_shape = tf.cast(shape * scale, tf.int32)
+    else:
+      new_shape = tf.cast([img_shape,img_shape], tf.int32)
+    pil_frame = tf.image.resize(pil_frame, new_shape)
+    # Append to dataset
+    frames.append(pil_frame)
+
+  frames = tf.convert_to_tensor(np.array(frames))
+  return frames
 
 def tensor_to_image(tensor):
   tensor = tensor*255
@@ -34,6 +86,18 @@ def save_img(image,img_name):
   plt.imshow(image)
   plt.axis('off')
   plt.savefig(img_name + '.png')
+
+def save_multiple_images(images, img_name):
+  fig = plt.figure(figsize=(4, 4))
+
+  for i in range(images.shape[0]):
+    plt.subplot(int(np.sqrt(images.shape[0])), int(np.sqrt(images.shape[0])), i + 1)
+    plt.imshow(images[i, :, :, :])
+    plt.axis('off')
+
+  # tight_layout minimizes the overlap between 2 sub-plots
+  plt.savefig(img_name + '.png')
+  # plt.show()
 
 
 def save_gif(filename, re_images_name='img*.png'):
@@ -58,3 +122,13 @@ def save_mp4(filename, re_images_name='img*.png'):
       writer.append_data(image)
     image = imageio.imread(filename)
     writer.append_data(image)
+
+
+def split_dataset(data, test_proportion=0.3):
+  test_size = int(np.shape(data)[0]*0.1)
+  test = data[:test_size]
+  train = data[test_size:]
+  return train, test
+
+def shuffle_dataset(data):
+  return tf.random.shuffle(data)
--- process_images.py
+++ process_images.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
+import numpy as np
 
 
 def clip_0_1(image):
   return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+def normalize_images(images):
+  # Normalize the image by 255
+  return tf.convert_to_tensor(np.array(images) / 255.0, dtype="float32")
